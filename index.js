7cf2a9d2363defd9b515a39d5da5698998823d16
Neil Johnston <neil@neiljohnston.net>
2018-08-25 09:51:35 -0700

    Initial api

--- index.js
+++ index.js
@@ -1,13 +1,8 @@
 const { send } = require('micro');
 const { router, get } = require('microrouter');
 const fetch = require('node-fetch');
-// const request = require("request");
-
-// const hello = async (req, res) => {
-//   send(res, 200, await Promise.resolve(`Hello ${req.params.who}`))};
-
-// const proxy = async (req, res) =>
-//   send(res, 200, await Promise.resolve(`Proxy ${req.params.url}`));
+const tj = require('@mapbox/togeojson');
+const DOMParser = require('xmldom').DOMParser;
 
 const smokeproxy = async (req, res) => {
   const response = await fetch('https://www.ospo.noaa.gov/data/land/fire/smoke.kml');
@@ -18,6 +13,20 @@ const smokeproxy = async (req, res) => {
   res.setHeader('Access-Control-Allow-Origin', '*');
   // res.setHeader('Content-Type', 'text/xml');
   send(res, 200, data);
+}
+
+const kmltogeojson = async (req, res) => {
+
+  const proxyURL = await Promise.resolve(`${req.params.url}`);
+  console.log(req.params.url);
+  const response = await fetch(proxyURL);
+  const kml = await response.text();
+  const kmlDom = new DOMParser().parseFromString(kml);
+
+  const converted = tj.kml(kmlDom, { styles: false });
+
+  res.setHeader('Access-Control-Allow-Origin', '*');
+  send(res, 200, converted);
 }
 
 
@@ -36,5 +45,6 @@ const notfound = (req, res) => send(res, 404, 'Not found route');
 module.exports = router(
   get('/proxy/:url', proxy),
   get('/smokeproxy', smokeproxy),
+  get('/kmltogeojson/:url', kmltogeojson),
   get('/*', notfound)
 );
