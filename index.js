b62ad9cac5cbe0fa0b9cef2e6c04e0768bab4055
Neil Johnston <neil@neiljohnston.net>
2018-08-25 13:37:02 -0700

    Integrate DriveBC

--- index.js
+++ index.js
@@ -4,6 +4,14 @@ const fetch = require('node-fetch');
 const tj = require('@mapbox/togeojson');
 const DOMParser = require('xmldom').DOMParser;
 
+// map severity levels of events to a color
+const severityColors = {
+  "MINOR": "#ffff00",
+  "MODERATE": "#f5a623",
+  "MAJOR": "#ff0000",
+};
+
+
 const smokeproxy = async (req, res) => {
   const response = await fetch('https://www.ospo.noaa.gov/data/land/fire/smoke.kml');
   const data = await response.text();
@@ -17,8 +25,7 @@ const smokeproxy = async (req, res) => {
 
 const kmltogeojson = async (req, res) => {
 
-  const proxyURL = await Promise.resolve(`${req.params.url}`);
-  console.log(req.params.url);
+  const proxyURL = await Promise.resolve(req.params.url);
   const response = await fetch(proxyURL);
   const kml = await response.text();
   const kmlDom = new DOMParser().parseFromString(kml);
@@ -31,8 +38,7 @@ const kmltogeojson = async (req, res) => {
 
 
 const proxy = async (req, res) => {
-  const proxyURL = await Promise.resolve(`${req.params.url}`);
-  console.log(req.params.url);
+  const proxyURL = await Promise.resolve(req.params.url);
   const request = await fetch(proxyURL);
   const data = await request.text();
 
@@ -41,10 +47,52 @@ const proxy = async (req, res) => {
 }
 
 const notfound = (req, res) => send(res, 404, 'Not found route');
+
+const drive511proxy = async (req, res) => {
+  const response = await fetch('http://api.open511.gov.bc.ca/events?format=json&status=ACTIVE&event_type=INCIDENT');
+  const data = await response.text();
+  var json = JSON.parse(data);
+  json = json.events; 
+  // // restructure DriveBC 511 API JSON to become geoJSON compliant
+  json = driveBCtoGeoJson(json);
+
+  res.setHeader('Content-Type', 'application/json');
+  res.setHeader('Access-Control-Allow-Origin', '*');
+  // // res.setHeader('Content-Type', 'text/xml');
+  send(res, 200, JSON.stringify(json));
+}
+
+function driveBCtoGeoJson(json){
+  var geoJson = {
+          "type": "FeatureCollection",
+          "features": [],
+      };
+  // restructure DriveBC 511 API JSON to become geoJSON compliant
+  for (var i = 0; i < json.length; i++) {
+      var event = json[i];
+
+      var feature = {
+          "type": "Feature"
+      };
+      feature.properties = event;
+      feature.geometry = event.geography;
+      delete event.geography;
+
+      // add color properties to the feature JSON
+      // Note that tokml's handling of styles is a bit busted, but this will generate the needed ids
+      feature.properties["marker-color"] = severityColors[event.severity];
+      feature.properties["stroke"] = severityColors[event.severity];
+      feature.properties['title'] = event.severity + ' ' + event.headline + ': ' + event.roads[0].name;
+
+      geoJson.features.push(feature);
+  };
+  return geoJson;
+}
  
 module.exports = router(
   get('/proxy/:url', proxy),
   get('/smokeproxy', smokeproxy),
   get('/kmltogeojson/:url', kmltogeojson),
+  get('/drive511proxy', drive511proxy),
   get('/*', notfound)
 );
