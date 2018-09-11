4074fd41525fc38386d03df0199649357b67fc2f
Neil Johnston <neil@neiljohnston.net>
2018-09-11 14:11:41 -0700

    working MongoDB Call

--- index.js
+++ index.js
@@ -4,9 +4,13 @@ const fetch = require('node-fetch');
 const tj = require('@mapbox/togeojson');
 const DOMParser = require('xmldom').DOMParser;
 const flatten = require('geojson-flatten');
-var simplify = require('simplify-geojson')
+const simplify = require('simplify-geojson');
 
+const MongoClient = require('mongodb').MongoClient;
+const assert = require('assert');
 
+const PASSWORD = encodeURI('AC1190!');
+const uri = `mongodb+srv://AdventureConditions:${PASSWORD}@adventureconditionscluster0-7honr.mongodb.net/test?retryWrites=true`;
 
 // map severity levels of events to a color
 const severityColors = {
@@ -19,9 +23,6 @@ const severityColors = {
 const smokeproxy = async (req, res) => {
   const response = await fetch('https://www.ospo.noaa.gov/data/land/fire/smoke.kml');
   const data = await response.text();
-
-  // return data
-
   res.setHeader('Access-Control-Allow-Origin', '*');
   // res.setHeader('Content-Type', 'text/xml');
   send(res, 200, data);
@@ -50,8 +51,6 @@ const proxy = async (req, res) => {
   res.setHeader('Access-Control-Allow-Origin', '*');
   send(res, 200, data);
 }
-
-const notfound = (req, res) => send(res, 404, 'Not found route');
 
 const drive511proxy = async (req, res) => {
   const response = await fetch('http://api.open511.gov.bc.ca/events?format=json&status=ACTIVE&event_type=INCIDENT');
@@ -109,12 +108,51 @@ const simplifygeojson = async (req, res) => {
   res.setHeader('Access-Control-Allow-Origin', '*');
   send(res, 200, simplified);
 }
- 
+
+const findDocuments = function(collection, findJson, callback) {
+  collection.find(findJson).toArray(function(err, docs) {
+    assert.equal(err, null);
+    console.log("Found the following records");
+    // console.log(docs);
+    callback(docs);
+  });
+}
+
+const mongo = async (req, res) => {
+
+  let client;
+
+  try {
+    // Use connect method to connect to the Server
+    client = await MongoClient.connect(uri);
+    console.log("Connected to Server");
+
+    const db = client.db("adventureConditions");
+    const collection = db.collection("aggregationSources");
+
+    let documents = await collection.find({'groupId': 'fire-perimeters'}).toArray();
+
+    res.setHeader('Content-Type', 'application/json');
+    res.setHeader('Access-Control-Allow-Origin', '*');
+    send(res, 200, documents);
+
+  } catch (err) {
+    console.log(err.stack);
+  }
+
+  if (client) {
+    client.close();
+  }
+}
+
+const notfound = (req, res) => send(res, 404, 'Not found route');
+
 module.exports = router(
   get('/proxy/:url', proxy),
   get('/smokeproxy', smokeproxy),
   get('/kmltogeojson/:url', kmltogeojson),
   get('/simplifygeojson/:url', simplifygeojson),
   get('/drive511proxy', drive511proxy),
+  get('/mongo', mongo),
   get('/*', notfound)
 );
